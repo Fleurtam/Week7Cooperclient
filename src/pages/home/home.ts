66affbda90ac3da7c39d727c6b9c35e2e674035f
FleurTamboezer <ftamboezer@gmail.com>
2018-03-10 15:10:33 +0100

    Deleted double button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { PerfomanceDataProvider } from '../../providers/perfomance-data/perfomance-data';
 import { PersonProvider } from '../../providers/person/person';
+import { ResultsPage } from '../results/results';
+
+
 
 @Component({
   selector: 'page-home',
@@ -11,10 +16,19 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
+    public modalCtrl: ModalController,
     public person: PersonProvider
+    // public perfomanceData: PerfomanceDataProvider
   ) {
     this.user = { distance: 1000, age: 20, gender: 'female' };
   }
+
+  // calculate(user) {
+  //   this.person.doAssessment(user.distance);
+  //   this.perfomanceData
+  //     .saveData({ performance_data: { data: { message: this.person.assessmentMessage } } })
+  //     .subscribe(data => console.log(data));
+  // }
 
   calculate() {
     this.person.age = this.user.age;
@@ -22,5 +36,9 @@ export class HomePage {
 
     this.person.doAssessment(this.user.distance);
     console.log(this.person.assessmentMessage);
+  }
+
+  showResults() {
+    this.modalCtrl.create(ResultsPage).present();
   }
 }
