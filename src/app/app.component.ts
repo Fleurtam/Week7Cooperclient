dc36043fbf2b50a4e24cde111fa426a23c3718b8
FleurTamboezer <ftamboezer@gmail.com>
2018-03-10 14:21:41 +0100

    Added login method for user Added login and logout buttons Added Angular2TokenService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 // import { ListPage } from '../pages/list/list';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   templateUrl: 'app.html'
 })
-
-
 
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
@@ -18,9 +17,22 @@ export class MyApp {
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
+  currentUser: any;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    private _tokenService: Angular2TokenService
+  ) {
+    this._tokenService.init({
+      apiBase: 'https://ob-cooper-api.herokuapp.com/api/v1'
+    });
+
     this.initializeApp();
+
+  // constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  //   this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
@@ -30,6 +42,55 @@ export class MyApp {
 
   }
 
+  loginPopUp() {
+      console.log('popup');
+      let confirm = this.alertCtrl.create({
+        title: 'Login',
+        inputs: [
+          {
+            name: 'email',
+            placeholder: 'email'
+          },
+          {
+            name: 'password',
+            placeholder: 'password',
+            type: 'password'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Login',
+            handler: data => {
+              this.login(data);
+            }
+          }
+        ]
+      });
+      confirm.present();
+    }
+
+    login(credentials) {
+      this._tokenService
+        .signIn(credentials)
+        .subscribe(
+        res => (this.currentUser = res.json().data),
+        err => console.error('error')
+        );
+    }
+
+    logout() {
+      this._tokenService
+        .signOut()
+        .subscribe(res => console.log(res), err => console.error('error'));
+      this.currentUser = undefined;
+    }
+  
 
   initializeApp() {
     this.platform.ready().then(() => {
